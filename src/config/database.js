3b67a8745eb43e61f9b6b3b95004897ef6ae43af
Ruan Linos Alves <ruandeveloper@hotmail.com>
2020-04-05 15:24:02 -0300

    initialized a production db connect

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,26 @@
 require('dotenv/config');
 
 module.exports = {
-  dialect: 'postgres',
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  define: {
-    timestamps: true,
-    underscored: true,
+  development: {
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    define: {
+      timestamps: true,
+      underscored: true,
+    },
+  },
+  production: {
+    dialect: 'postgres',
+    host: process.env.DB_PROD_HOST,
+    username: process.env.DB_PROD_USER,
+    password: process.env.DB_PROD_PASS,
+    database: process.env.DB_PROD_NAME,
+    define: {
+      timestamps: true,
+      underscored: true,
+    },
   },
 };
--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,11 @@
 const Sequelize = require('sequelize');
 const dbConfig = require('../config/database');
-
+require('dotenv/config');
 const Tool = require('../models/Tool');
 
-const connection = new Sequelize(dbConfig);
+const connection = process.env.NODE_ENV === 'development'
+  ? new Sequelize(dbConfig.development)
+  : new Sequelize(dbConfig.production);
 
 Tool.init(connection);
 
