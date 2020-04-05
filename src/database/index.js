da67d8c1261a23d42774fd4ab2c869018cef7bd8
Ruan Linos Alves <ruandeveloper@hotmail.com>
2020-04-05 17:21:24 -0300

    fix: fixed sequelize connection with heroku and postgres

--- src/database/index.js
+++ src/database/index.js
@@ -1,13 +1,21 @@
+// @ts-nocheck
+/* eslint-disable no-undef */
 const Sequelize = require('sequelize');
 const dbConfig = require('../config/database');
-// @ts-ignore
+
 require('dotenv/config');
 const Tool = require('../models/Tool');
 
 const connection = process.env.NODE_ENV === 'production'
-  // @ts-ignore
-  ? new Sequelize(process.env.DATABASE_URL)
-  // @ts-ignore
+  ? new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+
+    port: match[4],
+
+    host: match[3],
+    logging: true, // false
+  })
   : new Sequelize(dbConfig.development);
 
 Tool.init(connection);
