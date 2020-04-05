bf853824640504731abdaa520ac259d2edd334cd
Ruan Linos Alves <ruandeveloper@hotmail.com>
2020-04-05 15:43:43 -0300

    fix: fixed database production

--- src/config/database.js
+++ src/config/database.js
@@ -14,7 +14,7 @@ module.exports = {
   },
   production: {
     dialect: 'postgres',
-    host: process.env.DB_PROD_HOST,
+    host: process.env.DATABASE_URL,
     username: process.env.DB_PROD_USER,
     password: process.env.DB_PROD_PASS,
     database: process.env.DB_PROD_NAME,
