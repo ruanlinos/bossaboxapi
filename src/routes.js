8e410a178663cf7f1d9e43a540114f046c179c4e
Ruan Linos Alves <ruandeveloper@hotmail.com>
2020-04-05 18:39:59 -0300

    added a route for test

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const ToolsController = require('./Controllers/ToolsController');
 
 const routes = express.Router();
-routes.get('/', () => 'HI');
+routes.get('/teste', (req, res) => res.status(200).json({ message: 'hi' }));
 routes.get('/tools', ToolsController.index);
 routes.put('/tools/:id', ToolsController.update);
 routes.post('/tools', ToolsController.store);
